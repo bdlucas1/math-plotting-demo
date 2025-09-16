5ef4c8dcec4fecb880be8e8c03e363f1002f4206
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-09-16 08:44:10 -0400

    Update fe.py

--- plotting-plotly+dash/fe.py
+++ plotting-plotly+dash/fe.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 import threading
 import time
 
@@ -48,15 +49,15 @@ testgc2 = """
 
 demos = [
     #                                                                                  eval  layout   total (ms)
-    dp3d,   # Plot3D Sin 50x50           current, generates G3D slowly                10026     181   10207
+    #dp3d,   # Plot3D Sin 50x50           current, generates G3D slowly                10026     181   10207
     dp3dv1, # Plot3Dv1 Sin/Sqrt 200x200  send Plot3D unmodified to layout                 0      26      26
-    dp3dv2, # Plot3Dv2 Sin/Sqrt 200x200  G3D, individual polys, no GraphicsComplex      433     622    1055
-    dp3dv3, # Plot3Dv2 Sin/Sqrt 200x200  G3D, GraphicsComplex, numpy_array_list_expr    252     247     495
+    #dp3dv2, # Plot3Dv2 Sin/Sqrt 200x200  G3D, individual polys, no GraphicsComplex      433     622    1055
+    #dp3dv3, # Plot3Dv2 Sin/Sqrt 200x200  G3D, GraphicsComplex, numpy_array_list_expr    252     247     495
     dp3dv4, # Plot3Dv2 Sin/Sqrt 200x200  G3D, GraphicsComplex, NumpyArrayListExpr                           
-    testgc1,
-    testgc2,
-    dmp3ds, # Man Plot3Dv1 Sin/Sqrt 200x200
-    dmp3dh, # Man Plot3Dv1 HypGeo 200x200
+    #testgc1,
+    #testgc2,
+    #dmp3ds, # Man Plot3Dv1 Sin/Sqrt 200x200
+    #dmp3dh, # Man Plot3Dv1 HypGeo 200x200
 ]
 
 #
@@ -155,6 +156,11 @@ class ShellFrontEnd(DashFrontEnd):
         # TODO: add s as title
 
         for s in demos:
+
+            p = re.sub("[ \n]+", " ", s)
+            if len(p) > 80:
+                p = p[0:80] + "..."
+            print("===", p)
 
             expr = self.session.parse(s)
 
