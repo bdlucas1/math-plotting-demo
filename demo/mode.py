8ec46483b90c09b290b66c844043c952ab70f26d
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-09-30 15:34:53 -0400

    Default widgets per environment

--- demo/mode.py
+++ demo/mode.py
@@ -1,13 +1,13 @@
 import os
 
-use = os.getenv("DEMO_USE", "").split(",")
-
+# in jupyter?
 try:
     __IPYTHON__
     in_jupyter = True
 except:
     in_jupyter = False
 
+# in jupyter_lite?
 try:
     import pyodide
     in_jupyterlite = True
@@ -15,13 +15,39 @@ except:
     in_jupyterlite = False
 
 
-# TODO: should this be harcoded?
-if "ipy" in use:
-    from mode_ipy import *
+# defaults
+if in_jupyter:
+    use_widgets = "ipy"
 else:
+    use_widgets = "dash"
+
+
+# override with env variable    
+use = os.getenv("DEMO_USE", "").split(",")
+if "ipy" in use:
+    use_widgets = "ipy"
+elif "dash" in use:
+    use_widgets = "dash"
+
+
+# which widgets to use    
+if use_widgets == "ipy":
+    from mode_ipy import *
+    requires = ["ipywidgets", "plotly"]
+elif use_widgets == "dash":
     from mode_dash import *
+    requires = []
 
+# in jupyterlite install requirements
+# TODO: this would require an async function for it to be called from
+# not sure how to do that (or if it's possible) while importing modules
+async def install_requirements():
+    import piplite
+    for r in requires:
+        print("installing", r)
+        await piplite.install(r)
 
+# stub fe for jupyter
 if in_jupyter:
 
     # FE stub - just wraps a session
