b4da160171e648a957321f90735ed00132cb9956
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-10-10 14:32:18 -0400

    Update ex.py

--- demo/ex.py
+++ demo/ex.py
@@ -19,85 +19,88 @@ def get_rule_values(expr):
     for rule in get_rules(expr):
         yield rule.elements[0], rule.elements[1].to_python()
 
-# instantiate an actual nested List structure from a numpy array
-# this is slow
-def numpy_array_list_expr(v, mathics_type):
-    #print("xxx nale", mathics_type)
-    if isinstance(v, np.ndarray):
-        return mcs.ListExpression(*(numpy_array_list_expr(vv, mathics_type) for vv in v), literal_values = v)
-    else:
-        # numpy scalar as mathics type
-        return mathics_type(v.item())
-
 #
-# quacks like a List Expression, but only lazily generates elements if you really insist
-# those in the know can look at .value, which is an np array, for an efficient shortcut
-# that avoids ever constructing the List per se
-# TODO: what else is needed to make this really quack like a List Expression
+# Lazily evaluated list expressions
+# copied from WIP update to mathics-core list.py
 #
 
-class LazyListExpression(mcs.ListExpression):
+import abc
+from typing import Sequence, Tuple, cast
 
-    def __init__(self, value):
+from mathics.core.list import ListExpression
+from mathics.core.element import BaseElement
 
-        super().__init__(None, literal_values = value)
+
+class LazyListExpression(ListExpression, abc.ABC):
+
+    """
+    A ListExpression with a supplied value that represents a list in
+    some way, but no supplied elements. The elements will be
+    instantiated on demand from the value. This allows the value to be
+    stored and used efficiently but at the same to present a normal
+    ListExpression view if required.
+    """
+
+    def __init__(self, value: Sequence) -> None:
+
+        # we are a ListExpression with no .elements but a .value
+        super().__init__(literal_values = value)
 
         # will be lazily computed if requested
         # subclass must provide self._make_elements for this purpose
-        self.__elements = None
+        self.__elements: Tuple[BaseElement, ...] | None = None
+
+    @abc.abstractmethod
+    def _make_elements(self) -> Tuple[BaseElement, ...]:
+        pass
+
+    # TODO: pyrefly gives us side-eye here becase we're overriding a member attribute with a property
+    # (but mypy and pyright do not complain...)
+    # the justification I turned up with a little googling was that properties can violate the substitution principle
+    # because they can change performance or semantics (e.g. by raising exceptions)
+    # personally I don't buy it because the same is true of any overriden method, and if that is a concern of the type
+    # system then exceptions and performance characteristics should be part of the type signatures of attributes and methods
+    # and the ability to slide in a property to replace an attribute is kind of a core benefit of properties
 
     @property
-    def _elements(self):
+    def _elements(self) -> Tuple[BaseElement, ...]:
         if not self.__elements:
-            self._make_elements()
+            self.__elements = self._make_elements()
         return self.__elements
 
     @_elements.setter
-    def _elements(self, e):
+    def _elements(self, e) -> None:
         self.__elements = e
 
+    # this is primarily for testing
+    @property
+    def is_instantiated(self) -> bool:
+        return self.__elements is not None
 
-# python complex to mathics Complex
-# does this already exist somewhere?
-def py_to_m_complex(v):
-    return mcs.Complex(mcs.Real(v.real), mcs.Real(v.imag))
 
-np_to_m = {t: mcs.Integer for t in ("int8", "int16", "int32", "int64", "uint8", "uint16", "uint32", "uint64")}
-np_to_m |= {t: mcs.Real for t in ("float16", "float32", "float64")}
-np_to_m |= {t: py_to_m_complex for t in ("complex64", "complex128")}
 
 class NumpyArrayListExpression(LazyListExpression):
 
-    def __init__(self, value):
+    """
+    A lazily instantiated ListExpression backed by a numpy array.
+    This allows data to be efficiently stored, transmitted, and
+    accessed efficiently as a numpy array by a collaborating source
+    and recipient, only instantiating the inefficent elements
+    representation if required by some third party.
+    """
 
-        super().__init__(value)
+    def __init__(self, value: np.ndarray):
+        # TODO: np.ndarray is not assignable to Sequence because nominal vs structural, but I think this is safe
+        # also since .value is declared Sequence typechecker will complain if user tries to modify the array
+        # even though arrays allow it; I guess this is a good thing
+        super().__init__(cast(Sequence,value))
 
-        # compute mathics type corresponding to np array type
-        # do this up front so we fail early on unsupported types
-        try:
-            self.np_to_m = np_to_m[str(value.dtype)]
-        except:
-            raise TypeError(f"Unsupported numpy type {value.dtype}")
-
-    # lazy computation of elements
-    def _make_elements(self):
-        def np_to_m(v):
+    # lazy computation of elements from numpy array
+    def _make_elements(self) -> Tuple[BaseElement, ...]:
+        def np_to_m(v) -> BaseElement | NumpyArrayListExpression:
             if isinstance(v, np.ndarray):
                 return NumpyArrayListExpression(v)
             else:
-                return self.np_to_m(v.item())
-        self._elements = [np_to_m(v) for v in self.value]
-
-
-if __name__ == "__main__":
-
-    a = np.array([1, 2, 3])
-    b = np.array([[1, 2], [3, 4]])
-    c = np.array([[17.5, 18.5], [2.2, 3.3]])
-    d = np.array([17j+3])
-
-    for x in [a, b, c, d]:
-        l = NumpyArrayListExpression(x)
-        print(l.np_to_m, l)
-
-
+                return from_python(v.item())
+        return tuple(np_to_m(v) for v in self.value)
+    
