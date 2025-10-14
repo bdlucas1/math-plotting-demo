30cb2c1b6f412e2a445b8e5be5fcc13f77c75067
Bruce Lucas <bdlucas1@users.noreply.github.com>
2025-10-14 13:05:15 -0400

    Plotting demos

--- demo/demos/plots.m
+++ demo/demos/plots.m
@@ -4,12 +4,19 @@ Grid[{{
         {{f,1.0}, 0.1, 2.0, 0.2}
     ]]
 }, {
-    Plot3D[
-        Sin[x]*Cos[y],
-        {x,0,10}, {y,0,10},
-        PlotPoints->{200,200},
-        Axes -> {True,True,True}
-    ]
+    Hold[Manipulate[
+        Plot3D[
+            Sin[x]*Cos[y]*a,
+            {x,0,10}, {y,0,10},
+            PlotPoints->{200,200},
+            Axes -> {True,True,True},
+            PlotRange -> {Automatic, Automatic, {-2,2}}
+        ],
+        {{a,1}, 0, 2, 0.1}
+    ]]
 }, {
-    NumberLinePlot[{1,3,4}]
+    Hold[Manipulate[
+        NumberLinePlot[{1,x,4}],
+        {{x,2}, 1.0, 4.0, 0.1}
+    ]]
 }}]
--- demo/fe.py
+++ demo/fe.py
@@ -21,6 +21,7 @@ parser.add_argument("--fe", choices=["shell", "browser", "jupyter"], default="sh
 parser.add_argument("--browser", choices=["webview", "webbrowser"], default="webview")
 parser.add_argument("--run", choices=["demos","tests","timing","dev"], default=None)
 parser.add_argument("--dev", type=str, default=None)
+parser.add_argument("demo", type=str, default=None, nargs="?")
 args = parser.parse_args()
 
 run = dict(
@@ -64,6 +65,9 @@ def read_demo(s):
 if args.dev:    
     run["dev"] = [args.dev]
     args.run = "dev"
+if args.demo:
+    run["demo"] = [args.demo]
+    args.run = "demo"
 if args.run:
     run[args.run] = [read_demo(s) for s in run[args.run]]
 
